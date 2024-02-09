192e3bdfb77055b459072fe17247688229770cdb
Aungkokolin1997 <lin@quartile.co>
2024-02-09 08:21:33 +0630

    [MIG] mail_hide_send_message: Migration to 16.0

--- mail_hide_send_message/__manifest__.py
+++ mail_hide_send_message/__manifest__.py
@@ -6,10 +6,10 @@
     "license": "AGPL-3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": ["mail"],
     "assets": {
-        "web.assets_qweb": [
+        "web.assets_backend": [
             "mail_hide_send_message/static/src/components/chatter_topbar/chatter_topbar.xml",
         ],
     },
