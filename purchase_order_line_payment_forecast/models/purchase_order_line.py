ab6737a27973fa184183991d6fda9919c8e1378a
Aungkokolin1997 <lin@quartile.co>
2024-07-22 04:11:05 +0000

    adj

--- purchase_order_line_payment_forecast/models/purchase_order_line.py
+++ purchase_order_line_payment_forecast/models/purchase_order_line.py
@@ -24,9 +24,14 @@ class PurchaseOrderLine(models.Model):
     def _compute_price_subtotal_billed(self):
         for line in self:
             if line.is_deposit:
-                line.price_subtotal_billed = line.price_unit
+                line.price_subtotal_billed = 0.0
                 line.price_subtotal_unbilled = (
-                    sum(line.invoice_lines.mapped("balance")) * -1
+                    sum(
+                        line.invoice_lines.filtered(
+                            lambda l: l.move_id.state != "cancel"
+                        ).mapped("balance")
+                    )
+                    * -1
                 )
                 continue
             line.price_subtotal_billed = line.price_unit * line.qty_invoiced
