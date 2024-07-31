9ed425b746fab38b406833c8082163a844f687e5
Aungkokolin1997 <lin@quartile.co>
2024-07-31 11:16:14 +0000

    add price_subtotal_unbilled_curr

--- purchase_order_line_payment_forecast/models/purchase_order_line.py
+++ purchase_order_line_payment_forecast/models/purchase_order_line.py
@@ -9,11 +9,26 @@ from odoo import api, fields, models
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
+    company_currency_id = fields.Many2one(
+        "res.currency",
+        related="company_id.currency_id",
+        string="Company Currency",
+        readonly=True,
+        store=True,
+    )
+    currency_rate = fields.Float(related="order_id.currency_rate")
     price_subtotal_billed = fields.Monetary(
         compute="_compute_price_subtotal_billed", store=True
     )
     price_subtotal_unbilled = fields.Monetary(
         compute="_compute_price_subtotal_billed", store=True
+    )
+    price_subtotal_unbilled_curr = fields.Monetary(
+        readonly=True,
+        help="Price Subtotal Unbilled Amount in the company Currency",
+        compute="_compute_price_subtotal_unbilled_curr",
+        currency_field="company_currency_id",
+        store=True,
     )
     payment_term_id = fields.Many2one(related="order_id.payment_term_id", store=True)
     expected_payment_date = fields.Date(
@@ -33,6 +48,15 @@ class PurchaseOrderLine(models.Model):
                 continue
             line.price_subtotal_billed = line.price_unit * line.qty_invoiced
             line.price_subtotal_unbilled = line.price_unit * line.qty_to_invoice
+
+    @api.depends("price_subtotal_unbilled", "currency_rate")
+    def _compute_price_subtotal_unbilled_curr(self):
+        for line in self:
+            if line.currency_id.id == line.company_id.currency_id.id:
+                unbilled_amount_curr = line.price_subtotal_unbilled
+            else:
+                unbilled_amount_curr = line.price_subtotal_unbilled / line.currency_rate
+            line.price_subtotal_unbilled_curr = unbilled_amount_curr
 
     @api.depends("date_planned", "payment_term_id")
     def _compute_expected_payment_date(self):
