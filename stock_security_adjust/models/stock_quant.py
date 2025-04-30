c792c4d8f088ffa111db2acc50823f6e948b78d1
nobuQuartile <nobu@quartile.co>
2025-04-30 06:47:33 +0000

    upd

--- stock_security_adjust/models/stock_quant.py
+++ stock_security_adjust/models/stock_quant.py
@@ -4,7 +4,4 @@ from odoo import fields, models
 class StockQuant(models.Model):
     _inherit = "stock.quant"
 
-    value = fields.Monetary(
-        compute="_compute_value",
-        groups="stock_security_adjust.group_stock_officer",
-    )
+    value = fields.Monetary(groups="stock_security_adjust.group_stock_officer")
