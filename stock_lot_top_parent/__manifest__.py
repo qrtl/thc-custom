c126a1f48df5fb2976bdfdae99d3f47e99792d57
nobuQuartile <nobu@quartile.co>
2026-07-28 08:28:39 +0000

    fixup

--- stock_lot_top_parent/__manifest__.py
+++ stock_lot_top_parent/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Lot Top Parent",
+    "summary": "Track the top-level parent serial, part number and current "
+    "physical location on lots/serials.",
     "version": "16.0.1.0.0",
     "category": "Inventory",
     "license": "AGPL-3",
     "author": "Quartile",
     "website": "https://www.quartile.co",
     "maintainers": ["nobuQuartile"],
-    "summary": "Track the top-level parent serial, part number and current "
-    "physical location on lots/serials.",
     "depends": ["stock"],
     "data": [
         "views/stock_lot_views.xml",
